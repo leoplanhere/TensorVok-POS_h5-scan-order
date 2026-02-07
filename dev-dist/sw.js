f3ed8ab55e05a217a44920b1b7710fe6877175ef
leo <sevena57@gmail.com>
2026-02-07 23:57:15 +0800

    readytogoonmac

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.0f6oqfa39m"
+    "revision": "0.55oqa6rc96g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
